6f57f1577cf33edc493195e6792015be6d46b16b
Terje Nesthus <terje@ekstremedia.no>
2024-08-20 03:45:21 +0200

    Update widgets

--- src/NetatmoWeatherServiceProvider.php
+++ src/NetatmoWeatherServiceProvider.php
@@ -2,35 +2,45 @@
 
 namespace Ekstremedia\NetatmoWeather;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Carbon;
 
 class NetatmoWeatherServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
 
         $this->publishes([
-            __DIR__.'/assets/images' => public_path('netatmo-weather/images'),
+            __DIR__ . '/assets/images' => public_path('netatmo-weather/images'),
 
         ], 'public');
 
         //        if (config('memory.modules.vehicle')) {
         //
         //        }
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations/');
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations/');
 
         $this->publishes([
-            __DIR__.'/config/netatmo-weather.php' => config_path('netatmo-weather.php'),
+            __DIR__ . '/config/netatmo-weather.php' => config_path('netatmo-weather.php'),
         ], 'config');
 
-        $this->loadViewsFrom(__DIR__.'/resources/views', 'netatmoweather');
+        $this->loadViewsFrom(__DIR__ . '/resources/views', 'netatmoweather');
 
-        $this->loadTranslationsFrom(__DIR__.'/resources/lang', 'netatmoweather');
+        $this->loadTranslationsFrom(__DIR__ . '/resources/lang', 'netatmoweather');
 
         $this->mergeConfigFrom(
-            __DIR__.'/config/netatmo-weather.php', 'netatmo-weather'
+            __DIR__ . '/config/netatmo-weather.php', 'netatmo-weather'
         );
+
+        Blade::directive('datetime', static function ($expression) {
+            return "<?php echo \Illuminate\Support\Carbon::createFromTimestamp($expression)->setTimezone(config('app.timezone'))->format('Y-m-d H:i'); ?>";
+        });
+
+        Blade::directive('time', static function ($expression) {
+            return "<?php echo \Illuminate\Support\Carbon::createFromTimestamp($expression)->setTimezone(config('app.timezone'))->format('H:i'); ?>";
+        });
     }
 
     public function register(): void
--- src/resources/views/netatmo/widgets/mainModule.blade.php
+++ src/resources/views/netatmo/widgets/mainModule.blade.php
@@ -1,20 +1,35 @@
 <div class="flex flex-col justify-left items-center gap-x-4 w-full gap-y-1">
     <div class="justify-center flex items-center">
         <img src="{{ asset('netatmo-weather/images/icons/station.svg') }}" alt="Netatmo Module Logo" width="50">
-        <div>
+        <div class="flex flex-col items-center justify-center">
+            <span class="text-2xl">
             {{ $module->module_name }}
+            </span>
+            <small>
+                Last update: @datetime($module->latestReading->dashboard_data['time_utc'])
+            </small>
         </div>
     </div>
-    <div class="flex w-full gap-x-5 justify-center">
+    <div class="flex w-full gap-x-7 justify-center">
         <div class="flex flex-col">
             Temperature:
             {{ $module->latestReading->dashboard_data['Temperature'] }} c
         </div>
         <div class="flex flex-col">
-            Min: {{ $module->latestReading->dashboard_data['min_temp'] }} c
+            <div>
+                Min: {{ $module->latestReading->dashboard_data['min_temp'] }} c
+                <small>
+                    (@time($module->latestReading->dashboard_data['date_min_temp']))
+                </small>
+            </div>
         </div>
         <div class="flex flex-col">
-            Max: {{ $module->latestReading->dashboard_data['max_temp'] }} c
+            <div>
+                Max: {{ $module->latestReading->dashboard_data['max_temp'] }} c
+                <small>
+                (@time($module->latestReading->dashboard_data['date_max_temp']))
+                </small>
+            </div>
         </div>
         <div class="flex flex-col">
             Trend:
