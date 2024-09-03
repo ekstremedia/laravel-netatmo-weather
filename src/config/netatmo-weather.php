4c2fec366d83b900573f37dfe25e4dd61b279dfe
Terje Nesthus <terje@ekstremedia.no>
2024-09-03 19:44:30 +0200

    App name settings

--- src/config/netatmo-weather.php
+++ src/config/netatmo-weather.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    'name' => 'Laravel Netatmo Weather',
     'netathmo_auth_url' => env('NETATMO_AUTH_URL', 'https://api.netatmo.com/oauth2/authorize'),
     'netathmo_token_url' => env('NETATMO_TOKEN_URL', 'https://api.netatmo.com/oauth2/token'),
     'netathmo_api_url' => env('NETATMO_API_URL', 'https://api.netatmo.com/api'),
